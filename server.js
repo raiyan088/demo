6dbc816d0aa17ac42c42a539a6491a752a776b03
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-24 06:25:30 +0600

    upload v1

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const puppeteer = require('puppeteer')
 
-console.log('success')
+;(async() => {
+
+    console.log('Browser Open')
+
+    let browser = await puppeteer.launch({
+        headless: true,
+        args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+    })
+
+    console.log('Page Open')
+
+    let page = await browser.newPage()
+
+    console.log('Page Load')
+
+    await page.goto("https://www.google.com")
+    console.log(await page.title())
+
+    await page.close()
+    await browser.close()
+
+    console.log('Browser Close')
+
+})()
