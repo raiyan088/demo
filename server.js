834e39b932b1cbbef7848e0239e24230ea86e9cd
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-02-02 06:26:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer')
 const express = require('express')
 const request = require('request')
 
+let browser = null
 
 const app = express()
 
@@ -31,9 +32,25 @@ app.get('/ip', async function (req, res) {
     })
 })
 
+app.get('/puppeteer', async function (req, res) {
+    if(browser == null) {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+        })
+        res.writeHeader(200, { "Content-Type": "text/html" })
+        res.write('Success')
+        res.end()
+    } else {
+        res.writeHeader(200, { "Content-Type": "text/html" })
+        res.write('Success v2')
+        res.end()
+    }
+})
 
 app.get('/', async function (req, res) {
     res.writeHeader(200, { "Content-Type": "text/html" })
     res.write('Invalid Request')
     res.end()
 })
+
